22283980b90abb08f3b81f883363d13d143906dc
taiwox1 <taiwoadedotunx1@gmail.com>
2023-11-08 22:34:34 +0100

    weather app

---
+++ src/api.jsx
@@ -0,0 +1,14 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+   'X-RapidAPI-Key': 'f9e4c9755dmshdfcb6e88ae63f3cp170852jsne2c88f39d30b',// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+
+
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "e661946526fa93261f8234e980e3ab78"; // enter your key from openweather API
