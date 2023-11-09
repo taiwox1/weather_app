3970a02a64216a816fba126f98ef68d713773688
taiwox1 <taiwoadedotunx1@gmail.com>
2023-11-09 22:50:21 +0100

    Made secure

--- src/api.jsx
+++ src/api.jsx
@@ -11,4 +11,4 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "e661946526fa93261f8234e980e3ab78"; // enter your key from openweather API
+export const WEATHER_API_KEY = ""; // enter your key from openweather API
